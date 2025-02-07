30be90d543ae468674ab781e29950f1606678e15
oloklkl <sungju7694@naver.com>
2025-02-07 14:04:22 +0900

    login 페이지 제작 중

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { LoginButtons, LoginContent, LoginHeader, LoginWrap } from './style';
-import { IconButton } from '../../ui';
-import { AppleLogo } from '@phosphor-icons/react';
+import { LoginButtons, LoginContent, LoginHeader, LoginNotice, LoginWrap, Logo } from './style';
 
 const Login = () => {
     return (
         <LoginWrap>
             <Link to={'/'}>
-                <img
+                <Logo
                     src='https://raw.githubusercontent.com/lse-7660/bokjak-image/1cc1bc51d66246dcc6e27bc9ed887e2759fba6d1/images/common/bokjak.svg'
                     alt='logo'
-                />
+                ></Logo>
             </Link>
             <LoginContent>
                 <LoginHeader>
@@ -21,26 +19,34 @@ const Login = () => {
                 <LoginButtons>
                     <button>
                         <img
-                            src='https://raw.githubusercontent.com/lse-7660/bokjak-image/18cb6ec19defe09b46ec12917c88c31e962632b7/images/login/%EC%B9%B4%EC%B9%B4%EC%98%A4%ED%86%A1.svg'
-                            alt=''
-                        ></img>
-                    </button>
-                    <button>
-                        <IconButton className='none' icon={<AppleLogo size={24} />} text='AppleLogo' />
+                            src='https://raw.githubusercontent.com/lse-7660/bokjak-image/61105440a091e532b03925524f6d2b5296cd750b/images/login/kakao.svg'
+                            alt='kakao'
+                        />
+                        <span>카카오로 시작하기</span>
                     </button>
                     <button>
                         <img
                             src='https://raw.githubusercontent.com/lse-7660/bokjak-image/18cb6ec19defe09b46ec12917c88c31e962632b7/images/login/%EB%84%A4%EC%9D%B4%EB%B2%84.svg'
-                            alt=''
-                        ></img>
+                            alt='apple'
+                        />
+                        <span>네이버로 시작하기</span>
                     </button>
                     <button>
                         <img
-                            src='https://raw.githubusercontent.com/lse-7660/bokjak-image/18cb6ec19defe09b46ec12917c88c31e962632b7/images/login/%EA%B5%AC%EA%B8%80.svg'
-                            alt=''
-                        ></img>
+                            src='https://raw.githubusercontent.com/lse-7660/bokjak-image/61105440a091e532b03925524f6d2b5296cd750b/images/login/apple.svg'
+                            alt='naver'
+                        />
+                        <span>Apple로 시작하기</span>
+                    </button>
+                    <button>
+                        <img
+                            src='https://raw.githubusercontent.com/lse-7660/bokjak-image/61105440a091e532b03925524f6d2b5296cd750b/images/login/google.svg'
+                            alt='google'
+                        />
+                        <span>구글로 시작하기</span>
                     </button>
                 </LoginButtons>
+                <LoginNotice>SNS계정으로 간편하게 가입하여 서비스를 이용하실 수 있습니다.</LoginNotice>
             </LoginContent>
         </LoginWrap>
     );
--- src/pages/login/style.js
+++ src/pages/login/style.js
@@ -1,23 +1,137 @@
 import styled from 'styled-components';
-import { color } from '../../styled/theme';
+import { color, font } from '../../styled/theme';
+import { media } from '../../styled/media';
 
 export const LoginWrap = styled.div`
+    position: relative;
     background-color: ${color('gray', '50')};
     margin: 80px 0;
-    img {
-        width: 140px;
-        height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* min-height: 100vh; */
+    ${media.tablet} {
+    }
+    ${media.mobile} {
+    }
+`;
+
+export const Logo = styled.img`
+    position: absolute;
+    top: 80px;
+    left: 160px;
+    width: 140px;
+    height: 40px;
+
+    ${media.tablet} {
+        top: 50px;
+        left: 60px;
+    }
+    ${media.mobile} {
+        top: 50px;
+        left: 20px;
     }
 `;
 
 export const LoginContent = styled.div`
-    background-color: ${color('gray', '30')};
+    background-color: ${color('gray', '70')};
+    padding: 40px 55px;
+    width: 600px;
+    height: 600px;
+
+    ${media.tablet} {
+        width: 470px;
+        height: 596px;
+    }
+    ${media.mobile} {
+        width: 345px;
+        height: 545px;
+    }
 `;
 
-export const LoginHeader = styled.div``;
+export const LoginHeader = styled.div`
+    h1 {
+        font-size: ${font('heading', 'sm')};
+        text-align: center;
+
+        ${media.tablet} {
+        }
+        ${media.mobile} {
+            font-size: ${font('heading', 'xs')};
+        }
+    }
+
+    h1:nth-of-type(2) {
+        margin-bottom: 40px;
+        ${media.tablet} {
+        }
+        ${media.mobile} {
+        }
+    }
+`;
 
 export const LoginButtons = styled.div`
-    botton {
-        color: #00000070;
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+
+    button {
+        width: 490px;
+        height: 60px;
+        display: flex;
+        align-items: center;
+        gap: 130px;
+        padding: 12px 20px;
+        background: #00000070;
+        border-radius: 7px;
+        border: 1px solid ${color('gray', '50')};
+
+        &:hover {
+            background: ${color('gray', '50')};
+            span {
+                color: ${color('gray', '0')};
+                font-weight: 700;
+            }
+        }
+
+        ${media.tablet} {
+            height: 58px;
+        }
+        ${media.mobile} {
+            width: 345px;
+            height: 60px;
+        }
+    }
+
+    img {
+        width: 24px;
+        height: 24px;
+    }
+
+    span {
+        /* flex: 1; */
+        text-align: center;
+        font-size: ${font('body', 'md')};
+        color: ${color('gray', '30')};
+
+        ${media.tablet} {
+        }
+        ${media.mobile} {
+            font-size: ${font('body', 'sm')};
+        }
+    }
+`;
+
+export const LoginNotice = styled.span`
+    display: block;
+    color: ${color('gray', '40')};
+    font-size: ${font('detail', 'md')};
+    margin-top: 30px;
+    text-align: center;
+
+    ${media.tablet} {
+    }
+    ${media.mobile} {
+        font-size: ${font('detail', 'sm')};
     }
 `;
