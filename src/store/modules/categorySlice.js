d13ace653eb78a15735564c46c4f63f27255b015
oloklkl <sungju7694@naver.com>
2025-02-11 20:50:03 +0900

    카테고리 상태관리를 위한 Redux 슬라이스 구현

--- src/store/modules/categorySlice.js
+++ src/store/modules/categorySlice.js
@@ -1,12 +1,43 @@
 import { createSlice } from '@reduxjs/toolkit';
 // 카테고리별 콘텐츠 페이지
-const initialState = {};
+const initialState = {
+    categories: [
+        '코미디',
+        '공포',
+        '판타지',
+        '무협',
+        'SF',
+        '예능',
+        '다큐멘터리',
+        '가족',
+        '키즈',
+        '액션',
+        '로맨스',
+        '스릴러',
+        '어드벤처',
+        '성인',
+        '미스터리',
+        '범죄',
+        '애니메이션',
+        '한국 TV 프로그램',
+        '외국 TV 프로그램',
+    ],
+    isOpen: false,
+    selectedCategory: null,
+};
 
 export const categorySlice = createSlice({
     name: 'category',
     initialState,
-    reducers: {},
+    reducers: {
+        toggleCategory: (state) => {
+            state.isOpen = !state.isOpen;
+        },
+        selectCategory: (state, action) => {
+            state.selectedCategory = action.payload;
+        },
+    },
 });
 
-export const categoryActions = categorySlice.actions;
+export const { toggleCategory, selectCategory } = categorySlice.actions;
 export default categorySlice.reducer;
