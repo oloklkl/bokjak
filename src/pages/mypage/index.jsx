87142c3919e7edf4c12ef1a2613cb471540c0794
oloklkl <sungju7694@naver.com>
2025-02-13 00:39:18 +0900

    제작 중: mypage

--- src/pages/mypage/index.jsx
+++ src/pages/mypage/index.jsx
@@ -1,5 +1,16 @@
+import BottomNavigation from '../../common/bottomnavigation';
+import ProfileSection from '../../components/profile';
+import { BarButton } from '../../ui';
+import { MyPageWrap } from './style';
+
 const MyPage = () => {
-    return <div></div>;
+    return (
+        <MyPageWrap>
+            <ProfileSection />
+            <BarButton className='share' text='로그아웃' width='500px' height='50px' />
+            <BottomNavigation />
+        </MyPageWrap>
+    );
 };
 
 export default MyPage;
