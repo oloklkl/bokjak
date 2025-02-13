4072cd78324a28057111580a93f671af6bbc264b
oloklkl <sungju7694@naver.com>
2025-02-14 02:29:52 +0900

    제작 중: upcomingNotification  컴포넌트

--- src/components/notification/upcomingNotification/UpcomingNotificationItem.jsx
+++ src/components/notification/upcomingNotification/UpcomingNotificationItem.jsx
@@ -11,6 +11,10 @@ const UpcomingContentWrap = styled.div`
     justify-content: space-between;
 
     ${media.mobile} {
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+        align-items: flex-start;
     }
 `;
 
@@ -21,7 +25,7 @@ const HeaderTitle = styled.div`
         width: 40%;
     }
     ${media.mobile} {
-        width: 80%;
+        width: 100%;
     }
 
     h2 {
@@ -58,10 +62,11 @@ const UpcomingInfoWrap = styled.div`
     width: 17%;
 
     ${media.tablet} {
-        width: 25%;
+        width: 50%;
     }
     ${media.mobile} {
-        width: 80%;
+        width: 140%;
+        transform: translateY(20px);
     }
 
     h3 {
@@ -123,6 +128,17 @@ const UpcomingIconWrap = styled.div`
     }
 `;
 
+const FlexWrap = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    ${media.mobile} {
+        display: flex;
+        align-items: baseline;
+    }
+`;
+
 const UpcomingNotificationItem = () => {
     return (
         <UpcomingNotificationItemWrap>
@@ -130,35 +146,37 @@ const UpcomingNotificationItem = () => {
                 <HeaderTitle>
                     <h2>2월1일</h2>
                 </HeaderTitle>
-                <UpcomingImageWrap>
-                    <img
-                        src='https://github.com/oloklkl/bokjak-image/blob/main/images/main/flipback/flipcard1.png?raw=true'
-                        alt=''
-                    />
-                </UpcomingImageWrap>
-                <UpcomingInfoWrap>
-                    <UpcomingActionWrap>
-                        <h3>제목</h3>
-                        <AgeLabel text='ALL' />
-                    </UpcomingActionWrap>
-                    <em>·</em>
-                    <span>2017</span>
-                    <em>·</em>
-                    <span>액션</span>
-                    <em>·</em>
-                    <span>SF</span>
-                    <em>·</em>
-                    <span>해외영화</span>
-                </UpcomingInfoWrap>
-                <UpcomingDescription>
-                    <p>
-                        시빌 워 당시 토니 스타크에게 발탁되어 대단한 활약을 펼친 스파이더맨 피터 파커. 허세와 정의감으로
-                        똘똘 뭉친 그는 세상을 위협하는 강력한 적 처벌에 맞서려 한다.
-                    </p>
-                </UpcomingDescription>
-                <UpcomingIconWrap>
-                    <BellSimpleRinging size={24} weight='fill' />
-                </UpcomingIconWrap>
+                <FlexWrap>
+                    <UpcomingImageWrap>
+                        <img
+                            src='https://github.com/oloklkl/bokjak-image/blob/main/images/main/flipback/flipcard1.png?raw=true'
+                            alt=''
+                        />
+                    </UpcomingImageWrap>
+                    <UpcomingInfoWrap>
+                        <UpcomingActionWrap>
+                            <h3>스파이더맨 : 파 프롬 홈</h3>
+                            <AgeLabel text='ALL' />
+                        </UpcomingActionWrap>
+                        <em>·</em>
+                        <span>2017</span>
+                        <em>·</em>
+                        <span>액션</span>
+                        <em>·</em>
+                        <span>SF</span>
+                        <em>·</em>
+                        <span>해외영화</span>
+                    </UpcomingInfoWrap>
+                    <UpcomingDescription>
+                        <p>
+                            시빌 워 당시 토니 스타크에게 발탁되어 대단한 활약을 펼친 스파이더맨 피터 파커. 허세와
+                            정의감으로 똘똘 뭉친 그는 세상을 위협하는 강력한 적 처벌에 맞서려 한다.
+                        </p>
+                    </UpcomingDescription>
+                    <UpcomingIconWrap>
+                        <BellSimpleRinging size={24} weight='fill' />
+                    </UpcomingIconWrap>
+                </FlexWrap>
             </UpcomingContentWrap>
         </UpcomingNotificationItemWrap>
     );
