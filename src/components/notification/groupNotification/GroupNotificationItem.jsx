bf986a2a2be8a81c9739513aa6923618f93f1f6d
oloklkl <sungju7694@naver.com>
2025-02-13 14:56:29 +0900

    제작 중: groupNotification 컴포넌트

--- src/components/notification/groupNotification/GroupNotificationItem.jsx
+++ src/components/notification/groupNotification/GroupNotificationItem.jsx
@@ -1,5 +1,90 @@
+import { BellSimpleRinging, Link } from '@phosphor-icons/react';
+import { BarButton } from '../../../ui';
+import { GroupNotificationItemWrap } from '../style';
+import styled from 'styled-components';
+import { color, font } from '../../../styled/theme';
+
+const GroupNotificationImage = styled.div`
+    position: relative;
+    width: 300px;
+    height: 430px;
+
+    &::before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: #00000070;
+    }
+`;
+
+const GroupNotificationContent = styled.div`
+    width: 300px;
+    height: 100%;
+    padding: 16px;
+    box-sizing: border-box;
+
+    h2 {
+        font-size: ${font('title', 'md')};
+        font-weight: 700;
+        position: absolute;
+        top: 62%;
+        left: 20px;
+    }
+    span {
+        color: ${color('gray', '30')};
+        font-size: ${font('body', 'md')};
+        position: absolute;
+        top: 70%;
+        left: 140px;
+    }
+    span:nth-of-type(1) {
+        position: absolute;
+        top: 70%;
+        left: 20px;
+    }
+`;
+
+const TextWrap = styled.div``;
+
+const IconWrap = styled.div`
+    position: absolute;
+    top: 20px;
+    right: -45px;
+`;
+
+const ButtonWrap = styled.div`
+    position: absolute;
+    top: 78%;
+    margin-top: auto;
+`;
+
 const GroupNotificationItem = () => {
-    return <div></div>;
+    return (
+        <GroupNotificationItemWrap>
+            <GroupNotificationImage>
+                <img
+                    src='https://github.com/oloklkl/bokjak-image/blob/main/images/main/flipback/flipcard1.png?raw=true'
+                    alt=''
+                />
+            </GroupNotificationImage>
+            <GroupNotificationContent>
+                <IconWrap>
+                    <BellSimpleRinging size={24} weight='fill' />
+                </IconWrap>
+                <TextWrap>
+                    <h2>콘텐츠 제목</h2>
+                    <span>파티방 제목</span>
+                    <span>시간</span>
+                </TextWrap>
+                <ButtonWrap>
+                    <BarButton icon={<Link />} text='공유' width='265px' height='42px' />
+                </ButtonWrap>
+            </GroupNotificationContent>
+        </GroupNotificationItemWrap>
+    );
 };
 
 export default GroupNotificationItem;
--- src/components/notification/groupNotification/GroupNotificationList.jsx
+++ src/components/notification/groupNotification/GroupNotificationList.jsx
@@ -1,5 +1,105 @@
+import { CaretLeft, CaretRight } from '@phosphor-icons/react';
+import {
+    GroupNotificationHeader,
+    GroupNotificationListWrap,
+    GroupNotificationSlider,
+    NavigationButton,
+} from '../style';
+import { IconButton } from '../../../ui';
+import { useRef } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import GroupNotificationItem from './GroupNotificationItem';
+import styled from 'styled-components';
+import { font } from '../../../styled/theme';
+import { media } from '../../../styled/media';
+
+const HeaderContent = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    text-align: center;
+    gap: 1400px;
+    h2 {
+        font-size: ${font('title', 'exlg')};
+        width: 200px;
+
+        ${media.tablet} {
+            gap: 620px;
+            font-size: ${font('title', 'xlg')};
+        }
+        ${media.mobile} {
+            gap: 320px;
+            font-size: ${font('title', 'lg')};
+        }
+    }
+    h3 {
+        font-size: ${font('body', 'sm')};
+    }
+`;
+
 const GroupNotificationList = () => {
-    return <div></div>;
+    const swiperRef = useRef();
+
+    const goNext = () => {
+        // **추가됨: goNext 함수**
+        if (swiperRef.current && swiperRef.current.swiper) {
+            swiperRef.current.swiper.slideNext();
+        }
+    };
+
+    const goPrev = () => {
+        // **추가됨: goPrev 함수**
+        if (swiperRef.current && swiperRef.current.swiper) {
+            swiperRef.current.swiper.slidePrev();
+        }
+    };
+
+    return (
+        <GroupNotificationListWrap>
+            <GroupNotificationHeader>
+                <HeaderContent>
+                    <h2>모여봐요! 알림</h2>
+                    <h3>more</h3>
+                </HeaderContent>
+            </GroupNotificationHeader>
+            <GroupNotificationSlider>
+                <Swiper
+                    className='swiper'
+                    ref={swiperRef}
+                    modules={[Navigation]}
+                    pagination={{ clickable: true }}
+                    spaceBetween={24}
+                    slidesPerGroup={6}
+                    slidesPerView={6}
+                    navigation={false}
+                >
+                    <SwiperSlide>
+                        <GroupNotificationItem />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <GroupNotificationItem />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <GroupNotificationItem />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <GroupNotificationItem />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <GroupNotificationItem />
+                    </SwiperSlide>
+                </Swiper>
+
+                <NavigationButton>
+                    <IconButton onClick={goPrev} className='b30' icon={<CaretLeft size={24} />} text='caretLeft' />
+                    <IconButton onClick={goNext} className='b30' icon={<CaretRight size={24} />} text='caretRight' />
+                </NavigationButton>
+            </GroupNotificationSlider>
+        </GroupNotificationListWrap>
+    );
 };
 
 export default GroupNotificationList;
