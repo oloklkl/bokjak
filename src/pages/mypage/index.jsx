fe13a6165c9ab757498a23b6a8580c70d600f0e7
oloklkl <sungju7694@naver.com>
2025-02-18 03:03:58 +0900

    mypage 컴포넌트 변경/ inner 추가

--- src/pages/mypage/index.jsx
+++ src/pages/mypage/index.jsx
@@ -1,5 +1,6 @@
 import BottomNavigation from '../../common/bottomnavigation';
-import { ThumbnailCont, ViewHistoryCont } from '../../components';
+import { ThumbnailCont } from '../../components';
+import MyPageViewHistoryCont from '../../components/mypage/MyPageViewHistoryCont';
 import ProfileSection from '../../components/profile';
 import { BarButton } from '../../ui';
 import { MyPageWrap } from './style';
@@ -7,11 +8,13 @@ import { MyPageWrap } from './style';
 const MyPage = () => {
     return (
         <MyPageWrap>
-            <ProfileSection />
+            <div className='inner'>
+                <ProfileSection />
 
-            <ViewHistoryCont />
-            <ThumbnailCont />
-            <BarButton className='share' text='로그아웃' width='500px' height='50px' />
+                <MyPageViewHistoryCont />
+                <ThumbnailCont />
+                <BarButton className='share' text='로그아웃' width='500px' height='50px' />
+            </div>
             <BottomNavigation />
         </MyPageWrap>
     );
