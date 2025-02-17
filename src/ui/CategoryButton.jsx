9f88b48ed3104401c6fff1eb9a96274f487735af
oloklkl <sungju7694@naver.com>
2025-02-17 10:42:00 +0900

    CategoryButton을 react-router-dom Link로 교체 및 active 상태 스타일 적용

--- src/ui/CategoryButton.jsx
+++ src/ui/CategoryButton.jsx
@@ -1,43 +1,39 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import { color, font } from '../styled/theme';
 import { media } from '../styled/media';
-
-const CategoryBtnWrap = styled.button`
+const StyledLink = styled(Link)`
     display: flex;
-    flex-direction: row;
     align-items: center;
     justify-content: center;
     gap: 10px;
-
     padding: 0 46px;
     height: 60px;
     border-radius: 9999px;
-    background-color: ${color('gray', '80')};
-    color: ${color('gray', '30')};
-
     font-size: ${font('title', 'lg')};
-
+    background-color: ${({ isActive }) => (isActive ? color('gray', '0') : color('gray', '80'))};
+    color: ${({ isActive }) => (isActive ? color('gray', '100') : color('gray', '30'))};
+    border: ${({ isActive }) => (isActive ? `1px solid ${color('gray', '0')}` : 'none')};
+    text-decoration: none;
     &:hover {
         background-color: ${color('gray', '70')};
         color: ${color('gray', '0')};
         border: 1px solid ${color('gray', '0')};
     }
-
     ${media.tablet} {
-        padding: 0 46px;
         height: 50px;
         font-size: ${font('detail', 'lg')};
     }
-
     ${media.mobile} {
-        padding: 0 30px;
         height: 40px;
         font-size: ${font('detail', 'sm')};
     }
 `;
-
-const CategoryButton = (props) => {
-    return <CategoryBtnWrap>{props.text}</CategoryBtnWrap>;
+const CategoryButton = ({ text, to, isActive }) => {
+    return (
+        <StyledLink to={to} isActive={isActive}>
+            {text}
+        </StyledLink>
+    );
 };
-
 export default CategoryButton;
