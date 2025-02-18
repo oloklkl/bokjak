a78579f225eb6c71dbd07544d7cea97760188e71
oloklkl <sungju7694@naver.com>
2025-02-18 21:46:56 +0900

    mypage: 무작위 섞기를 제거하고 movies와 tvShows에서 첫 10개 항목만 표시

--- src/pages/mypage/index.jsx
+++ src/pages/mypage/index.jsx
@@ -9,7 +9,7 @@ import { MyPageWrap } from './style';
 const MyPage = () => {
     const { movies, tvShows } = useSelector((state) => state.contentR);
 
-    const combinedContents = [...movies, ...tvShows].sort(() => Math.random() - 0.5).slice(0, 10);
+    const combinedContents = [...movies, ...tvShows].slice(0, 10);
 
     return (
         <MyPageWrap>
@@ -18,6 +18,7 @@ const MyPage = () => {
 
                 <MyPageViewHistoryCont />
                 <SubThumbnailCont title='관심 콘텐츠' contents={combinedContents} />
+
                 <BarButton className='share' text='로그아웃' width='500px' height='50px' />
             </div>
             <BottomNavigation />
