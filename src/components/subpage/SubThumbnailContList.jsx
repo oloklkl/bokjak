1c2e5fb96e446b6ae305a3e3f3981f4a0ad30168
oloklkl <sungju7694@naver.com>
2025-02-18 12:00:59 +0900

    SubThumbnailContList에 title prop 추가

---
+++ src/components/subpage/SubThumbnailContList.jsx
@@ -0,0 +1,99 @@
+import { CaretLeft, CaretRight } from '@phosphor-icons/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import { useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getContentByGenre, getContentDetail, getMovies, getTvShows } from '../../store/modules/getThunk';
+import { Link, useLocation } from 'react-router-dom';
+import { ThumbnailContainer, ThumbnailHeader, ThumbnailList } from './style';
+import ThumbnailCard from '../../ui/ThumbnailCard';
+import { NavigationButton } from './style';
+import { IconButton } from '../../ui';
+
+const SubThumbnailContList = ({ title, contents }) => {
+    const { movies } = useSelector((state) => state.contentR);
+    // const { movies, tvShows } = useSelector((state) => state.contentR)
+    const dispatch = useDispatch();
+    const location = useLocation();
+
+    useEffect(() => {
+        dispatch(getMovies());
+        dispatch(getTvShows());
+    }, [dispatch]);
+
+    const showDetailModal = (type, id, genreId) => {
+        dispatch(getContentDetail({ type, id }));
+        dispatch(getContentByGenre({ type, genreId }));
+    };
+
+    const swiperRef = useRef();
+
+    const goNext = () => {
+        swiperRef.current?.swiper.slideNext();
+    };
+    const goPrev = () => {
+        swiperRef.current?.swiper.slidePrev();
+    };
+
+    return (
+        <ThumbnailContainer>
+            <ThumbnailHeader>
+                <h2>{title}</h2>
+                <Link>
+                    <h3>더보기</h3>
+                </Link>
+            </ThumbnailHeader>
+            <ThumbnailList>
+                <Swiper
+                    className='swiper'
+                    ref={swiperRef}
+                    modules={[Navigation]}
+                    pagination={{ clickable: true }}
+                    navigation={false}
+                    breakpoints={{
+                        330: {
+                            slidesPerView: 2.8,
+                            slidesPerGroup: 1,
+                            spaceBetween: 10,
+                        },
+                        390: {
+                            slidesPerView: 3.1,
+                            slidesPerGroup: 1,
+                            spaceBetween: 10,
+                        },
+                        768: {
+                            slidesPerView: 4.2,
+                            slidesPerGroup: 1,
+                            spaceBetween: 16,
+                        },
+                        1024: {
+                            slidesPerView: 5.1,
+                            slidesPerGroup: 5,
+                            spaceBetween: 24,
+                        },
+                    }}
+                >
+                    {movies.map((content) => (
+                        <SwiperSlide key={content.id}>
+                            <Link to={`/movie/${content.id}`} state={{ previousLocation: location }}>
+                                <ThumbnailCard
+                                    content={content}
+                                    onClick={() => showDetailModal('movie', content.id, content.genre_ids)}
+                                />
+                            </Link>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+
+                <NavigationButton>
+                    <IconButton onClick={goPrev} className='b30' icon={<CaretLeft size={24} />} text='caretLeft' />
+                    <IconButton onClick={goNext} className='b30' icon={<CaretRight size={24} />} text='caretRight' />
+                </NavigationButton>
+            </ThumbnailList>
+        </ThumbnailContainer>
+    );
+};
+
+export default SubThumbnailContList;
