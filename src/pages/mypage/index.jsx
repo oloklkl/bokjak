a08fb5299620970eefd1f0b0d734632495b5e901
oloklkl <sungju7694@naver.com>
2025-02-18 21:02:05 +0900

    MyPage에서 useSelector로 영화와 드라마 콘텐츠 결합 및 랜덤 콘텐츠 표시

--- src/pages/mypage/index.jsx
+++ src/pages/mypage/index.jsx
@@ -1,18 +1,23 @@
+import { useSelector } from 'react-redux';
 import BottomNavigation from '../../common/bottomnavigation';
-import { ThumbnailCont } from '../../components';
 import MyPageViewHistoryCont from '../../components/mypage/MyPageViewHistoryCont';
 import ProfileSection from '../../components/profile';
+import SubThumbnailCont from '../../components/subpage/SubThumbnailCont';
 import { BarButton } from '../../ui';
 import { MyPageWrap } from './style';
 
 const MyPage = () => {
+    const { movies, tvShows } = useSelector((state) => state.contentR);
+
+    const combinedContents = [...movies, ...tvShows].sort(() => Math.random() - 0.5).slice(0, 10);
+
     return (
         <MyPageWrap>
             <div className='inner'>
                 <ProfileSection />
 
                 <MyPageViewHistoryCont />
-                <ThumbnailCont />
+                <SubThumbnailCont title='관심 콘텐츠' contents={combinedContents} />
                 <BarButton className='share' text='로그아웃' width='500px' height='50px' />
             </div>
             <BottomNavigation />
