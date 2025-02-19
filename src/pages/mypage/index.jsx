74e784e3167acbe888c8d789e5385c57589d2de5
oloklkl <sungju7694@naver.com>
2025-02-19 23:10:01 +0900

    MyPage: 로그아웃 시 로그인 페이지 이동

--- src/pages/mypage/index.jsx
+++ src/pages/mypage/index.jsx
@@ -1,4 +1,6 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { authActions } from '../../store/modules/authSlice';
+import { useNavigate } from 'react-router-dom';
 import BottomNavigation from '../../common/bottomnavigation';
 import MyPageViewHistoryCont from '../../components/mypage/MyPageViewHistoryCont';
 import ProfileSection from '../../components/profile';
@@ -7,9 +9,18 @@ import { BarButton } from '../../ui';
 import { MyPageWrap } from './style';
 
 const MyPage = () => {
+    const dispatch = useDispatch();
+    dispatch(authActions.logout());
+
+    const navigate = useNavigate();
     const { movies, tvShows } = useSelector((state) => state.contentR);
 
     const combinedContents = [...movies, ...tvShows].slice(0, 10);
+
+    const handleLogout = () => {
+        dispatch(authActions.logout());
+        navigate('/login');
+    };
 
     return (
         <MyPageWrap>
@@ -19,7 +30,7 @@ const MyPage = () => {
                 <MyPageViewHistoryCont />
                 <SubThumbnailCont title='관심 콘텐츠' contents={combinedContents} />
 
-                <BarButton className='share' text='로그아웃' width='500px' height='50px' />
+                <BarButton className='share' text='로그아웃' width='500px' height='50px' onClick={handleLogout} />
             </div>
             <BottomNavigation />
         </MyPageWrap>
