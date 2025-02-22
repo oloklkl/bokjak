b03fae662a6004752086c7872184ea1c0d484adc
lse-7660 <lse76609251@gmail.com>
2025-02-22 18:56:58 +0900

    introList export 수정

--- src/components/main/intro/Intro.jsx
+++ src/components/main/intro/Intro.jsx
@@ -1,17 +1,11 @@
-import { Suspense } from 'react';
 import IntroList from './introList';
 import { IntroSection } from './style';
 
 const Intro = () => {
-    const IntroList = React.lazy(() =>
-        import('./introList')
-    );
     return (
         <IntroSection>
             <div className="inner">
-                <Suspense fallback={<div>Loading...</div>}>
-                    <IntroList />
-                </Suspense>
+                <IntroList />
             </div>
         </IntroSection>
     );
--- src/components/main/intro/IntroList.jsx
+++ src/components/main/intro/IntroList.jsx
@@ -1,79 +1,96 @@
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { Autoplay, EffectCreative } from 'swiper/modules'
-import { useDispatch, useSelector } from 'react-redux'
-import IntroItem from './IntroItem'
-import { IntroSliderCont } from './style'
-import { Link, useLocation } from 'react-router-dom'
-import { useEffect } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, EffectCreative } from 'swiper/modules';
+import { useDispatch, useSelector } from 'react-redux';
+import IntroItem from './IntroItem';
+import { IntroSliderCont } from './style';
+import { Link, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import {
-  getContentByGenre,
-  getContentDetail,
-  getMovies,
-  getTvShows,
-} from '../../../store/modules/getThunk'
+    getContentByGenre,
+    getContentDetail,
+    getMovies,
+    getTvShows,
+} from '../../../store/modules/getThunk';
 
 const IntroList = () => {
-  const { movies } = useSelector((state) => state.contentR)
-  // const { movies, tvShows } = useSelector((state) => state.contentR)
-  const dispatch = useDispatch()
-  const location = useLocation()
-  const intro = movies.slice(0, 7)
+    const { movies } = useSelector(
+        (state) => state.contentR
+    );
+    // const { movies, tvShows } = useSelector((state) => state.contentR)
+    const dispatch = useDispatch();
+    const location = useLocation();
+    const intro = movies.slice(0, 7);
 
-  useEffect(() => {
-    dispatch(getMovies())
-    dispatch(getTvShows())
-  }, [dispatch])
+    useEffect(() => {
+        dispatch(getMovies());
+        dispatch(getTvShows());
+    }, [dispatch]);
 
-  const showDetailModal = (type, id, genreId) => {
-    dispatch(getContentDetail({ type, id }))
-    dispatch(getContentByGenre({ type, genreId }))
-  }
-  return (
-    <IntroSliderCont>
-      <Swiper
-        className="swiper"
-        modules={[Autoplay, EffectCreative]}
-        spaceBetween={24}
-        slidesPerView={1}
-        autoplay={{ delay: 6000, disableOnInteraction: false }}
-        loop={true}
-        effect="creative"
-        creativeEffect={{
-          prev: {
-            opacity: 1,
-            translate: ['-80%', 0, -50],
-            scale: 0.9,
-          },
-          next: {
-            opacity: 0.9,
-            translate: ['80%', 0, -100],
-            scale: 1,
-          },
-        }}
-        breakpoints={{
-          390: { spaceBetween: 12 },
-          768: { spaceBetween: 16 },
-          1024: { spaceBetween: 24 },
-        }}>
-        {intro
-          .filter((content) => content.overview?.trim() !== '')
-          .map((content) => (
-            <SwiperSlide key={content.id}>
-              <Link
-                to={`/movie/${content.id}`}
-                state={{ previousLocation: location }}>
-                <IntroItem
-                  content={content}
-                  onClick={() => {
-                    showDetailModal('movie', content.id, content.genre_ids)
-                  }}
-                />
-              </Link>
-            </SwiperSlide>
-          ))}
-      </Swiper>
-    </IntroSliderCont>
-  )
-}
+    const showDetailModal = (type, id, genreId) => {
+        dispatch(getContentDetail({ type, id }));
+        dispatch(getContentByGenre({ type, genreId }));
+    };
+    return (
+        <IntroSliderCont>
+            <Swiper
+                className="swiper"
+                modules={[Autoplay, EffectCreative]}
+                spaceBetween={24}
+                slidesPerView={1}
+                autoplay={{
+                    delay: 6000,
+                    disableOnInteraction: false,
+                }}
+                loop={true}
+                effect="creative"
+                creativeEffect={{
+                    prev: {
+                        opacity: 1,
+                        translate: ['-80%', 0, -50],
+                        scale: 0.9,
+                    },
+                    next: {
+                        opacity: 0.9,
+                        translate: ['80%', 0, -100],
+                        scale: 1,
+                    },
+                }}
+                breakpoints={{
+                    390: { spaceBetween: 12 },
+                    768: { spaceBetween: 16 },
+                    1024: { spaceBetween: 24 },
+                }}
+            >
+                {intro
+                    .filter(
+                        (content) =>
+                            content.overview?.trim() !== ''
+                    )
+                    .map((content) => (
+                        <SwiperSlide key={content.id}>
+                            <Link
+                                to={`/movie/${content.id}`}
+                                state={{
+                                    previousLocation:
+                                        location,
+                                }}
+                            >
+                                <IntroItem
+                                    content={content}
+                                    onClick={() => {
+                                        showDetailModal(
+                                            'movie',
+                                            content.id,
+                                            content.genre_ids
+                                        );
+                                    }}
+                                />
+                            </Link>
+                        </SwiperSlide>
+                    ))}
+            </Swiper>
+        </IntroSliderCont>
+    );
+};
 
-export default IntroList
+export default IntroList;
