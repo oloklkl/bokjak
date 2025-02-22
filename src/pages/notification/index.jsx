89d10ab539c120be6245ddc94bda8036ed5ac1e5
lse-7660 <lse76609251@gmail.com>
2025-02-22 18:37:02 +0900

    upComingNotification 위치 점검

--- src/pages/notification/index.jsx
+++ src/pages/notification/index.jsx
@@ -1,29 +1,33 @@
-import { Container, ContentWrapper, NotificationSection } from './style';
+import {
+    Container,
+    ContentWrapper,
+    NotificationSection,
+} from './style';
 
 import BottomNavigation from '../../common/bottomnavigation';
 import ProfileSection from '../../components/profile';
 import GroupNotification from '../../components/notification/groupNotification/GroupNotification';
-import UpcomingNotification from '../../components/notification/upcomingNotification/upcomingNotification';
+import UpcomingNotification from '../../components/notification/upcomingNotification/UpcomingNotification';
 
 const Notification = () => {
-  return (
-    <Container>
-      <ProfileSection />
+    return (
+        <Container>
+            <ProfileSection />
 
-      <ContentWrapper>
-        <div className="inner">
-          <NotificationSection>
-            <GroupNotification />
-          </NotificationSection>
+            <ContentWrapper>
+                <div className="inner">
+                    <NotificationSection>
+                        <GroupNotification />
+                    </NotificationSection>
 
-          <NotificationSection>
-            <UpcomingNotification />
-          </NotificationSection>
-        </div>
-      </ContentWrapper>
-      <BottomNavigation />
-    </Container>
-  );
+                    <NotificationSection>
+                        <UpcomingNotification />
+                    </NotificationSection>
+                </div>
+            </ContentWrapper>
+            <BottomNavigation />
+        </Container>
+    );
 };
 
 export default Notification;
