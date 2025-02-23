199678405ad26a66664505066965230e52e589be
oloklkl <sungju7694@naver.com>
2025-02-24 02:13:11 +0900

    notification 페이지: 로그인 전 화면 추가/ 스타일 수정

--- src/pages/notification/style.js
+++ src/pages/notification/style.js
@@ -1,7 +1,25 @@
 import styled from 'styled-components';
+import { color, font } from '../../styled/theme';
+import { media } from '../../styled/media';
 
 export const Container = styled.div`
     width: 100%;
+`;
+
+export const LoginPrompt = styled.div`
+    p {
+        margin-top: 100px;
+        color: ${color('gray', '30')};
+        font-size: ${font('heading', 'md')};
+
+        ${media.tablet} {
+            font-size: ${font('heading', 'sm')};
+        }
+        ${media.mobile} {
+            font-size: ${font('heading', 'xs')};
+            margin-bottom: 20px;
+        }
+    }
 `;
 
 export const ContentWrapper = styled.div``;
