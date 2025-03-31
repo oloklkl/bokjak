4f5ccd6f69dbeb3e7aae0217131825e29f9cf1c2
lse-7660 <lse76609251@gmail.com>
2025-03-31 14:37:36 +0900

    [feat] movies 콘텐츠 로고 이미지 있는 객체만 필터링하여 복작 로고 뜨지 않는 문제 해결

--- src/components/main/mainBokjak/BokjakContList.jsx
+++ src/components/main/mainBokjak/BokjakContList.jsx
@@ -1,43 +1,70 @@
 import BokjakContItem from './BokjakContItem';
 import { IconButton } from '../../../ui';
-import { CaretLeft, CaretRight, QuestionMark } from '@phosphor-icons/react';
+import {
+    CaretLeft,
+    CaretRight,
+    QuestionMark,
+} from '@phosphor-icons/react';
 import { useEffect, useRef, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { Navigation } from 'swiper/modules';
-import { BokjakHeader, BokjakList, BokjakListWrap } from './style';
+import {
+    BokjakHeader,
+    BokjakList,
+    BokjakListWrap,
+} from './style';
 import { NavigationButton } from '../style';
 import { Link, useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { getContentByGenre, getContentDetail, getMovies, getTvShows } from '../../../store/modules/getThunk';
+import {
+    getContentByGenre,
+    getContentDetail,
+    getMovies,
+    getTvShows,
+} from '../../../store/modules/getThunk';
 import BokjakModal from './BokjakModal';
 
 const BokjakContList = () => {
-    const { movies } = useSelector((state) => state.contentR);
-    const { bokjakData } = useSelector((state) => state.mainR);
+    const { movies } = useSelector(
+        (state) => state.contentR
+    );
+    const { bokjakData } = useSelector(
+        (state) => state.mainR
+    );
 
     const dispatch = useDispatch();
     const location = useLocation();
-    const movie = movies.slice(0, 10);
-    const bokjakItems = bokjakData.slice(0, 10);
+    const movie = movies;
+    const bokjakItems = bokjakData;
 
     // combinedContent: 영화 데이터와 복작 데이터의 타이틀, 시간 결합
-    const combinedContent = movie.map((content, index) => {
-        const bokjakTitle = bokjakItems[index]?.bokjak_title || 'Default Title';
-        const bokjakTime = bokjakItems[index]?.bokjak_time || 'Default Time';
-        const bokjakPeople = bokjakItems[index]?.expected_participants || 'Default Time';
+    const combinedContent = movie
+        .filter((content) => content.logoImage)
+        .slice(0, 10)
+        .map((content, index) => {
+            const bokjakTitle =
+                bokjakItems[index]?.bokjak_title ||
+                'Default Title';
+            const bokjakTime =
+                bokjakItems[index]?.bokjak_time ||
+                'Default Time';
+            const bokjakPeople =
+                bokjakItems[index]?.expected_participants ||
+                'Default Time';
 
-        return {
-            ...content,
-            bokjak_title: bokjakTitle,
-            bokjak_time: bokjakTime,
-            expected_participants: bokjakPeople,
-        };
-    });
+            return {
+                ...content,
+                bokjak_title: bokjakTitle,
+                bokjak_time: bokjakTime,
+                expected_participants: bokjakPeople,
+            };
+        });
 
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [selectedContent, setSelectedContent] = useState(null);
+    const [selectedContent, setSelectedContent] =
+        useState(null);
 
     useEffect(() => {
         dispatch(getMovies());
@@ -74,13 +101,23 @@ const BokjakContList = () => {
             <BokjakHeader>
                 <div className="quesCont">
                     <h2>🐣 모여봐요 복작 </h2>
-                    <IconButton className="border" icon={<QuestionMark size={18} />} text="smiley" />
+                    <IconButton
+                        className="border"
+                        icon={<QuestionMark size={18} />}
+                        text="smiley"
+                    />
                     <div className="msgBox">
                         <div className="textarea">
-                            ‘모여보기 복작’은 친구들과 실시간으로 영화를 보며 채팅으로 소통하는 특별한 경험입니다.{' '}
+                            ‘모여보기 복작’은 친구들과
+                            실시간으로 영화를 보며 채팅으로
+                            소통하는 특별한 경험입니다.{' '}
                             <br />
-                            🎬혼자서 느끼기엔 아쉬운 순간들, 이제 함께 즐기며 더 특별하게 만들어보세요! 💬✨ 멀리 있어도
-                            함께하는 그 순간, ‘모여보기 복작’에서 실시간 채팅으로 감상을 나누며 경험해보세요.
+                            🎬혼자서 느끼기엔 아쉬운 순간들,
+                            이제 함께 즐기며 더 특별하게
+                            만들어보세요! 💬✨ 멀리 있어도
+                            함께하는 그 순간, ‘모여보기
+                            복작’에서 실시간 채팅으로 감상을
+                            나누며 경험해보세요.
                         </div>
                     </div>
                 </div>
@@ -117,23 +154,36 @@ const BokjakContList = () => {
                         },
                     }}
                 >
-                    {combinedContent
-                        .filter((content) => content.logoImage?.trim() !== '')
-                        .map((content) => (
-                            <SwiperSlide key={content.id}>
-                                <Link state={{ previousLocation: location }}>
-                                    <BokjakContItem
-                                        content={content}
-                                        onClick={() => {
-                                            showDetailModal('movie', content.id, content.genre_ids);
-                                            openModal(content);
-                                        }}
-                                    />
-                                </Link>
-                            </SwiperSlide>
-                        ))}
+                    {combinedContent.map((content) => (
+                        <SwiperSlide key={content.id}>
+                            <Link
+                                state={{
+                                    previousLocation:
+                                        location,
+                                }}
+                            >
+                                <BokjakContItem
+                                    content={content}
+                                    onClick={() => {
+                                        showDetailModal(
+                                            'movie',
+                                            content.id,
+                                            content.genre_ids
+                                        );
+                                        openModal(content);
+                                    }}
+                                />
+                            </Link>
+                        </SwiperSlide>
+                    ))}
+
                     <NavigationButton>
-                        <IconButton onClick={goPrev} className="b30" icon={<CaretLeft size={24} />} text="caretLeft" />
+                        <IconButton
+                            onClick={goPrev}
+                            className="b30"
+                            icon={<CaretLeft size={24} />}
+                            text="caretLeft"
+                        />
                         <IconButton
                             onClick={goNext}
                             className="b30"
@@ -143,7 +193,12 @@ const BokjakContList = () => {
                     </NavigationButton>
                 </Swiper>
             </BokjakList>
-            {isModalOpen && selectedContent && <BokjakModal content={selectedContent} closeModal={closeModal} />}
+            {isModalOpen && selectedContent && (
+                <BokjakModal
+                    content={selectedContent}
+                    closeModal={closeModal}
+                />
+            )}
         </BokjakListWrap>
     );
 };
