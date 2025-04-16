ce458d47b9df4e3c0ebebf5b2a8c904569ad5382
hyeonky <hyeon2762@naver.com>
2025-04-17 01:12:57 +0900

    작업) 콘텐츠 이미지 데이터 수정

--- src/assets/api/about/aboutUpdateContentList.js
+++ src/assets/api/about/aboutUpdateContentList.js
@@ -14,22 +14,25 @@ const updateContentList = [
   {
     id: 3,
     title: '백설공주',
-    desc: '눈보라가 몰아치던 겨울 밤 태어난 백설공주. 온정이 넘치던 왕국에서 모두의 사랑을 받았지만, 강력한 어둠의 힘으로 왕국을 빼앗은 여왕의 위협에 숲으로 도망친다. 마법의 숲에서 간신히 살아남은 백설공주는 신비로운 일곱 광부들과 만나게 되며 새로운 세상을 마주하고, 마음속 깊이 숨겨진 용기와 선한 힘을 깨닫게 된다. 그리고 마침내, 빼앗긴 왕국을 되찾기 위해 여왕과 맞서 싸우기로 결심하는데…',
+    desc: '자신의 진짜 힘을 알지 못한 백설공주는 어둠의 위협을 피해 숲으로 도망친다. 마법의 숲에서 신비로운 일곱 광부들과 만나 새로운 세상을 경험하며, 자신 안에 숨겨져 있던 용기와 선한 힘을 깨닫는다. 결국, 빼앗긴 왕국을 되찾기 위해 여왕과 맞서기로 결심하며, 진정한 힘을 발휘하게 된다.',
     img: 'https://github.com/lse-7660/bokjak-image/blob/main/images/about/updateCont3.png?raw=true',
   },
   {
     id: 4,
     title: '위키드',
+    desc: '자신의 힘을 발견하지 못한 엘파바와 자신의 본성을 발견하지 못한 글린다는 마법 같은 우정을 쌓아간다. 그러던 어느 날, 마법사의 초대를 받아 에메랄드 시티에 간 두 사람은 예기치 못한 위기를 겪는다.',
     img: 'https://github.com/lse-7660/bokjak-image/blob/main/images/main/intro/introSlide1.png?raw=true',
   },
   {
     id: 5,
-    title: '위키드',
+    title: '미키17',
+    desc: '미키는 자신의 존재 이유를 알지 못한 채, 얼어붙은 행성 니플헤임으로 파견된다. 소모품 대원으로서 죽음을 반복하며 살아가던 중, 뜻밖의 복제체 미키 18이 등장하며 시스템에 균열을 일으킨다. 두 미키는 점차 정체성과 생존을 둘러싼 혼란에 빠지고, 그 과정에서 미키는 자신이 진짜 누구인지를 마주하게 된다.',
     img: 'https://github.com/lse-7660/bokjak-image/blob/main/images/main/intro/introSlide1.png?raw=true',
   },
   {
     id: 6,
-    title: '위키드',
+    title: '마인크래프트',
+    desc: '네 명의 이방인은 어느 날 갑자기 포털을 통해 낯선 큐브 세계, 오버월드로 빨려 들어간다. 그곳에서 상상력과 창의력이 생존의 열쇠임을 깨닫고, 신비로운 장인 스티브의 도움을 받아 현실로 돌아가기 위한 여정을 떠난다. 좀비, 스켈레톤, 네더의 어둠 속에서 각자의 가치를 발견하며 진정한 용기를 얻고 성장해 나간다.',
     img: 'https://github.com/lse-7660/bokjak-image/blob/main/images/main/intro/introSlide1.png?raw=true',
   },
 ]
