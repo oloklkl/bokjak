2d39a2978aafc54c73a99722550c8db85345ffb7
oloklkl <sungju7694@naver.com>
2025-04-18 02:23:40 +0900

    IntroList.jsx : 스와이퍼 수정

--- src/components/main/intro/IntroList.jsx
+++ src/components/main/intro/IntroList.jsx
@@ -5,17 +5,10 @@ import IntroItem from './IntroItem';
 import { IntroSliderCont } from './style';
 import { Link, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
-import {
-    getContentByGenre,
-    getContentDetail,
-    getMovies,
-    getTvShows,
-} from '../../../store/modules/getThunk';
+import { getContentByGenre, getContentDetail, getMovies, getTvShows } from '../../../store/modules/getThunk';
 
 const IntroList = () => {
-    const { movies } = useSelector(
-        (state) => state.contentR
-    );
+    const { movies } = useSelector((state) => state.contentR);
     // const { movies, tvShows } = useSelector((state) => state.contentR)
     const dispatch = useDispatch();
     const location = useLocation();
@@ -33,16 +26,18 @@ const IntroList = () => {
     return (
         <IntroSliderCont>
             <Swiper
-                className="swiper"
+                className='swiper'
                 modules={[Autoplay, EffectCreative]}
-                spaceBetween={24}
-                slidesPerView={1}
+                spaceBetween={0} // 슬라이드 간 간격을 0으로 설정하여 양쪽 끝에 포스터가 보이게 함
+                slidesPerView='auto' // 자동으로 슬라이드 크기 조정
+                centeredSlides={true} // 가운데 정렬
+                loop={true} // 무한 루프 설정
+                initialSlide={0} // 첫 번째 슬라이드로 설정
                 autoplay={{
                     delay: 6000,
                     disableOnInteraction: false,
                 }}
-                loop={true}
-                effect="creative"
+                effect='creative'
                 creativeEffect={{
                     prev: {
                         opacity: 1,
@@ -62,27 +57,19 @@ const IntroList = () => {
                 }}
             >
                 {intro
-                    .filter(
-                        (content) =>
-                            content.overview?.trim() !== ''
-                    )
+                    .filter((content) => content.overview?.trim() !== '')
                     .map((content) => (
                         <SwiperSlide key={content.id}>
                             <Link
                                 to={`/movie/${content.id}`}
                                 state={{
-                                    previousLocation:
-                                        location,
+                                    previousLocation: location,
                                 }}
                             >
                                 <IntroItem
                                     content={content}
                                     onClick={() => {
-                                        showDetailModal(
-                                            'movie',
-                                            content.id,
-                                            content.genre_ids
-                                        );
+                                        showDetailModal('movie', content.id, content.genre_ids);
                                     }}
                                 />
                             </Link>
