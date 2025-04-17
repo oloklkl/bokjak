b5cedfc632707ebf4cce37a9ef1e803fbb2bef0c
oloklkl <sungju7694@naver.com>
2025-04-18 03:06:44 +0900

    notification 페이지 : 구조 수정

--- src/pages/notification/index.jsx
+++ src/pages/notification/index.jsx
@@ -9,32 +9,31 @@ import { useSelector } from 'react-redux';
 const Notification = () => {
     const isAuthed = useSelector((state) => state.authR.authed);
 
-    if (!isAuthed) {
-        return (
-            <LoginPrompt>
-                <div className='inner'>
-                    <p>🎉 로그인하고 멋진 알림을 만나보세요!</p>
-                </div>
-            </LoginPrompt>
-        );
-    }
-
     return (
         <Container>
-            <ProfileSection />
+            {isAuthed ? (
+                <>
+                    <ProfileSection />
+                    <ContentWrapper>
+                        <div className='inner'>
+                            <NotificationSection>
+                                <GroupNotification />
+                            </NotificationSection>
 
-            <ContentWrapper>
-                <div className='inner'>
-                    <NotificationSection>
-                        <GroupNotification />
-                    </NotificationSection>
-
-                    <NotificationSection>
-                        <UpcomingNotification />
-                    </NotificationSection>
-                </div>
-            </ContentWrapper>
-            <BottomNavigation />
+                            <NotificationSection>
+                                <UpcomingNotification />
+                            </NotificationSection>
+                        </div>
+                    </ContentWrapper>
+                </>
+            ) : (
+                <LoginPrompt>
+                    <div className='inner'>
+                        <p>🎉 로그인하고 멋진 알림을 만나보세요!</p>
+                    </div>
+                </LoginPrompt>
+            )}
+            <BottomNavigation /> {/* 로그인 여부와 관계없이 항상 표시 */}
         </Container>
     );
 };
