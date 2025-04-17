857e5434393c9bc3fc3fdc20b63e6fcb6e42cfc4
oloklkl <sungju7694@naver.com>
2025-04-18 02:23:12 +0900

    about 페이지 : bottomnavigation 컴포넌트 추가

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,14 +1,16 @@
-import AboutPage from '../../components/about/AboutPage'
-import { AboutWrap } from './style'
+import BottomNavigation from '../../common/bottomnavigation';
+import AboutPage from '../../components/about/AboutPage';
+import { AboutWrap } from './style';
 
 const About = () => {
-  return (
-    <>
-      <AboutWrap className="aboutWrap">
-        <AboutPage />
-      </AboutWrap>
-    </>
-  )
-}
+    return (
+        <>
+            <AboutWrap className='aboutWrap'>
+                <AboutPage />
+            </AboutWrap>
+            <BottomNavigation />
+        </>
+    );
+};
 
-export default About
+export default About;
