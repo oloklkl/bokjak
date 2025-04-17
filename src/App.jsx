657252f48969c1ffdd2b3197fbb49a0ba3dc6b36
oloklkl <sungju7694@naver.com>
2025-04-18 05:28:57 +0900

    App.jsx : 경로수정

--- src/App.jsx
+++ src/App.jsx
@@ -65,20 +65,15 @@ const App = () => {
                 <meta property='og:type' content='website' />
                 <meta property='og:title' content='조성주 • BOKJAK-OTT' />
                 <meta property='og:description' content='복작 팀 프로젝트' />
-                <meta
-                    property='og:image'
-                    content='https://github.com/oloklkl/bokjak-image/blob/main/images/meta/bokjak-meta.png?raw=true'
-                />
+                <meta property='og:image' content='https://bokjak-project.netlify.app/meta/bokjak-meta.jpg' />
+
                 <meta property='og:url' content='https://bokjak-project.netlify.app/' />
 
                 {/* <!-- Twitter Card --> */}
                 <meta name='twitter:card' content='summary_large_image' />
                 <meta name='twitter:title' content='조성주 • BOKJAK-OTT' />
                 <meta name='twitter:description' content='복작 팀 프로젝트' />
-                <meta
-                    name='twitter:image'
-                    content='https://github.com/oloklkl/bokjak-image/blob/main/images/meta/bokjak-meta.png?raw=true'
-                />
+                <meta name='twitter:image' content='https://bokjak-project.netlify.app/meta/bokjak-meta.jpg' />
             </Helmet>
             <WindowListener />
             <ThemeProvider theme={theme}>
